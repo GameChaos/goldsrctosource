d39fc8b77bb3d1c8bcb10868df4cb77829167dec
GameChaos <franzkits@gmail.com>
2023-08-31 19:36:53 +0300

    New debug build parameters

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ pushd "%~dp0"
 where /q cl
 IF ERRORLEVEL 1 call "\run_vcvarsall.bat"
 
-
-set CommonCompilerFlags=-nologo -Zi -FC -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 ..\code\win32_goldsrctosource.cpp
+REM 4244 is a useful warning
+set CommonCompilerFlags=-nologo -utf-8 -diagnostics:caret -Zi -Zc:inline -FC -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4820 -wd5045 -wd4061 -wd4244 ..\code\win32_goldsrctosource.cpp
 set CommonLinkerFlags= -OUT:goldsrctosource.exe -subsystem:console -incremental:no -opt:ref -opt:icf kernel32.lib shell32.lib
 
 timeit.exe _start
@@ -20,7 +20,7 @@ sokol-shdc.exe --input code/shaders/world.glsl --output code/shaders_compiled/wo
 sokol-shdc.exe --input code/shaders/wire.glsl --output code/shaders_compiled/wire.h %ShdcParams% --slang glsl330:hlsl4:metal_macos -b
 
 pushd build
-cl -Od -DGC_DEBUG -DDEBUG_GRAPHICS -RTC1 -fsanitize=address -MTd %CommonCompilerFlags% /link %CommonLinkerFlags% imgui/imgui.lib libvcruntimed.lib libucrtd.lib
+cl -Od -GS -sdl -guard:cf -RTCu -DGC_DEBUG -DDEBUG_GRAPHICS -RTC1 -fsanitize=address -MTd %CommonCompilerFlags% /link %CommonLinkerFlags% -guard:cf imgui/imgui.lib libvcruntimed.lib libucrtd.lib
 
 ) else (
 
